c1ed29bc7a3953f794eeff352f2987b7384b9d95
Phoenix Zhang <phoenix.nir@gmail.com>
2017-05-18 18:57:43 +0800

    init

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to job-migrate
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/job_migrate/core_test.clj
@@ -0,0 +1,7 @@
+(ns job-migrate.core-test
+  (:require [clojure.test :refer :all]
+            [job-migrate.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
